b4e177c4433394a428397c5df34864ac196e8c15
Charanlokesh22 <charanlokesh2918@gmail.com>
2026-02-03 21:05:06 +0530

    Create UrlService.java

---
+++ UrlService.java
@@ -0,0 +1,10 @@
+package com.company.urlshortener.service;
+
+import com.company.urlshortener.entity.UrlMapping;
+
+public interface UrlService {
+
+    UrlMapping shortenUrl(String longUrl);
+
+    String getOriginalUrl(String shortUrl);
+}
