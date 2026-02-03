84e7a27eac5e18b763e84877cb0aab3afac699f6
Charanlokesh22 <charanlokesh2918@gmail.com>
2026-02-03 21:04:56 +0530

    Create UrlRepository.java

---
+++ UrlRepository.java
@@ -0,0 +1,13 @@
+package com.company.urlshortener.repository;
+
+import com.company.urlshortener.entity.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<UrlMapping, Long> {
+
+    Optional<UrlMapping> findByShortUrl(String shortUrl);
+
+    Optional<UrlMapping> findByLongUrl(String longUrl);
+}
